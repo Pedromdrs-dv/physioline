7dc9bf9074b508981637922c17b9a32f83ea0b63
Pedro Medeiros <pedro60883@estudante.ifb.edu.br>
2024-08-04 20:48:16 -0300

    fix index.js

--- assets/src/index.js
+++ assets/src/index.js
@@ -29,10 +29,8 @@ var header = document.getElementById('header');
             }
         });
 
-        // Get the button
         let mybutton = document.getElementById("myBtn");
 
-        // When the user scrolls down 20px from the top of the document, show the button
         window.onscroll = function () { scrollFunction() };
 
         function scrollFunction() {
@@ -43,7 +41,6 @@ var header = document.getElementById('header');
             }
         }
 
-        // When the user clicks on the button, scroll to the top of the document
         function topFunction() {
             document.body.scrollTop = 0;
             document.documentElement.scrollTop = 0;
